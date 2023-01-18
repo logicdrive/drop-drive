18ec25015c0058f6d36fc19d157f76385b2012ab
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-18 11:39:39 +0000

    파일선택시 업로드시킬 파일의 URL을 얻을 수 있도록 추가

--- routers/api/v1/auth/user_auth.js
+++ routers/api/v1/auth/user_auth.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     // console.log(firebase_auth.currentUser) // 로그인한 유저명을 얻기
-    res.json({user_auth:null}) // 현재 유저가 가지고 있는 권한을 반환 [MOCK]
+    res.json({user_auth:"SIN"}) // 현재 유저가 가지고 있는 권한을 반환 [MOCK]
 })
 
 
