14e0dc2667089edb226bd0d165a926f5ccb3858f
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-19 06:04:06 +0000

    파일의 메타데이터 정보가 파이어스토어에 저장되도록 기능을 추가함

--- routers/api/v1/file.js
+++ routers/api/v1/file.js
@@ -1,4 +1,7 @@
 import express from "express"
+import { firebase_store } from "../../../module/firebase.js"
+import { collection, addDoc } from "firebase/firestore"
+import { v4 } from "uuid"
 
 const router = express.Router()
 
@@ -7,16 +10,32 @@ router.get('/', (req, res) => {
     res.send('[MOCK] 파일 다운로드 처리')
 })
 
-router.put('/', (req, res) => {
+router.put('/', async (req, res) => {
     try
-    {
-      console.log(req.body)
-      res.json({is_error:false})
+    { 
+      const USER_AUTH = "Sin"
+      const ACCEPT_FILE_EXTS = ["txt"]
+      
+      const [FILE_NAME, FILE_EXT] = req.body.file_name.toLowerCase().split(".")
+      if(!ACCEPT_FILE_EXTS.includes(FILE_EXT))
+      {
+        res.json({is_error:true, error_message:"Passed file's extension was not accepted."})
+        return
+      }
+
+      await addDoc(collection(firebase_store, "file_meta_datas"), {
+        "file_name":FILE_NAME,
+        "file_ext":FILE_EXT,
+        "file_uuid":v4(),
+        "owner":USER_AUTH
+      })
+      
+      res.json({is_error:false, error_message:""})
     }
     catch(e)
     {
       console.log(e)
-      res.json({is_error:true})
+      res.json({is_error:true, error_message:"Internal server error was occurred."})
     }
 })
 
