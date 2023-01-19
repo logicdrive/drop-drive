c6eff21c7a36125f62115dfe4dfdab9950923fea
최효승 <37767318+lucky6030@users.noreply.github.com>
2023-01-19 05:37:28 +0000

    로그인 기능 복구

--- routers/api/v1/auth/signin.js
+++ routers/api/v1/auth/signin.js
@@ -1,11 +1,54 @@
 import express from "express"
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth"
 
 const router = express.Router()
 
-
 router.post('/', (req, res) => {
-    res.send('[MOCK] 로그인 처리')
-})
 
+  const auth = getAuth()
+  const email = req.body.email
+  const password = req.body.password
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user
+      //console.log(user)
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+      console.log(errorMessage[errorCode])
+    })
+  
+  const login = async (email, password) => {
+    try {
+      const auth = getAuth()
+      const { user } = await signInWithEmailAndPassword(auth, email, password)
+      const { stsTokenManager, uid } = user
+      setAuthInfo({ uid, email, authToken: stsTokenManager })
+      
+    } catch ({ code, message }) {
+      const errorCode = error.code
+      const errorMessage = error.message
+      alert(errorMessage[code])
+    }
+  }
+
+  // const logout = async () => {
+  //   const isLogOut = window.confirm(authMessage['auth/logout-confirm']);
+  //   if (!isLogOut) return;
+
+  //   try {
+  //     const auth = getAuth();
+  //     await signOut(auth);
+  //     setAuthInfo(initialState);
+  //     navigate('/');
+  //   } catch ({ code, message }) {
+  //     alert(errorMessage[code]);
+  //   }
+  // };
+
+  res.send('[MOCK] 로그인 처리')
+})
 
 export default router
--- routers/api/v1/auth/signup.js
+++ routers/api/v1/auth/signup.js
@@ -1,11 +1,75 @@
 import express from "express"
+import { firebase_auth, firebase_store } from "../../../../module/firebase.js"
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from "firebase/auth"
+import { doc, setDoc } from "firebase/firestore"
 
 const router = express.Router()
 
-
 router.post('/', (req, res) => {
-    res.send('[MOCK] 회원가입 처리')
-})
+  //현재사용자 객체 받아오기
+  const auth = getAuth()
+  const email = req.body.email
+  const password1 = req.body.password1
+  const password2 = req.body.password2
+  const nickname = req.body.nickname
+  const name = req.body.name
 
+  const password = password1
+  //확인 비밀번호가 서로 다를때
+  if (password1 != password2)
+    res.redirect("/html/login.html")
+
+  createUserWithEmailAndPassword(auth, email, password)
+  .then(async (userCredential) =>  {
+    const user = userCredential.user
+    const currentUser = {
+        id : user.uid,
+        email : email,
+        nickname : nickname,
+        name : name,
+        emailVerified : user.emailVerified
+    }
+    await setDoc(doc(firebase_store, "users", currentUser.id), {
+        name: currentUser.name,
+        email: currentUser.email
+    })
+  })
+  .catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+    if (errorCode == 'auth/weak-password') 
+      alert('The password is too weak.')
+  
+    console.log(errorMessage[errorCode])
+  })
+
+  sendEmailVerification(auth.currentUser)
+  .then(() => {
+    console.log('email succesfully sent')
+  })
+  .catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+  
+    console.log(errorMessage[errorCode])
+  })
+
+  //이메일 인증으로 회원가입
+  const joinWithVerification = async (email, password) => {
+    try {
+      const auth = getAuth()
+      await createUserWithEmailAndPassword(auth, email, password)
+      await sendEmailVerification(auth.currentUser)
+    } catch (error) {
+      const errorMessage = error.message;
+      const code = error.code
+      console.log(errorMessage[code])
+    }
+  }
+
+  joinWithVerification(email, password)
+  res.redirect("/html/login.html")
+  //res.send('[MOCK] 회원가입 처리')
+})
 
 export default router
