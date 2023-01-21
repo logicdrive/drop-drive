dde2d736575eb7e2df00055178e62fbb7efe7c2a
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-21 16:18:12 +0000

    shared_link 부분의 라우터 이벤트 함수를 분리시키는 리팩토링

--- routers/api/v1/shared_link.js
+++ routers/api/v1/shared_link.js
@@ -1,19 +1,28 @@
 import express from "express"
+import Wrap from "../../../module/wrap.js"
+
+async function put_Router_callback(req, res)
+{
+    res.send('[MOCK] 특정 파일에 대한 공유링크를 생성하는 처리')
+}
+
+async function get_Router_callback(req, res)
+{
+  res.send('[MOCK] 특정 파일에 대한 공유링크를 얻는 처리')
+}
+
+async function delete_Router_callback(req, res)
+{
+  res.send('[MOCK] 특정 파일에 대한 공유링크를 제거하는 처리')
+}
+
+put_Router_callback = Wrap.Wrap_With_Try_Res_Promise(put_Router_callback)
+get_Router_callback = Wrap.Wrap_With_Try_Res_Promise(get_Router_callback)
+delete_Router_callback = Wrap.Wrap_With_Try_Res_Promise(delete_Router_callback)
 
 const router = express.Router()
-
-
-router.get('/', (req, res) => {
-    res.send('[MOCK] 특정 파일에 대한 공유링크를 얻는 처리')
-})
-
-router.put('/', (req, res) => {
-    res.send('[MOCK] 특정 파일에 대한 공유링크를 생성하는 처리')
-})
-
-router.delete('/', (req, res) => {
-    res.send('[MOCK] 특정 파일에 대한 공유링크를 제거하는 처리')
-})
-
+router.put('/', put_Router_callback)
+router.get('/', get_Router_callback)
+router.delete('/', delete_Router_callback)
 
 export default router
