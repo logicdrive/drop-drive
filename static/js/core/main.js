f719cd7c4d4aa7e22803bc94790c01ea96563396
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-21 13:29:10 +0000

    주어진 함수 전체를 감싸도록 Wrap  클래스 내 함수 수정

--- static/js/core/main.js
+++ static/js/core/main.js
@@ -16,16 +16,12 @@ async function update_Greeting_Message()
 /** 현재 유저가 소유하고 있는 파일 목록관련 UI를 업데이트시킴 */ 
 async function update_Owned_File_Names()
 {
-  await Wrap.Execute_With_Try_Alert_Promise(async () => {
-
-    const FILE_NAMES = await Rest_API.owned_File_Names()
-    if(FILE_NAMES.length == 0) return
-    
-    const OWNED_FILE_TABLE_SEL = document.querySelector("#owned_file_table")
-    const FILE_NAME_HTMLS = FILE_NAMES.map((file_name) => `<tr><td><div>${file_name}</div></td></tr>`)
-    OWNED_FILE_TABLE_SEL.innerHTML = FILE_NAME_HTMLS.join("\n")
-    
-  })
+  const FILE_NAMES = await Rest_API.owned_File_Names()
+  if(FILE_NAMES.length == 0) return
+  
+  const OWNED_FILE_TABLE_SEL = document.querySelector("#owned_file_table")
+  const FILE_NAME_HTMLS = FILE_NAMES.map((file_name) => `<tr><td><div>${file_name}</div></td></tr>`)
+  OWNED_FILE_TABLE_SEL.innerHTML = FILE_NAME_HTMLS.join("\n")
 }
 
 /** 파일 업로드 폼이 제출되어졌을 경우 실행되는 이벤트 함수 */
@@ -34,19 +30,14 @@ async function on_Upload_File_Form_Submited(e)
   e.preventDefault()
 
   const INPUT_FILE_SEL = document.querySelector("#upload_file_form input[type='file']")
-  if(INPUT_FILE_SEL.files.length == 0)
-  {
-    alert("Please select a file to upload")
-    return
-  }
+  if(INPUT_FILE_SEL.files.length == 0) throw new Error("Please select a file to upload")
 
-  await Wrap.Execute_With_Try_Alert_Promise(async () => {
-    
-    const UPLOADED_FILE_NAME = await Rest_API.upload_File_Object(INPUT_FILE_SEL.files[0])
-    alert(`The requested file '${UPLOADED_FILE_NAME}' was successfully uploaded !`)
-    await update_Owned_File_Names()
-    
-  })
+  const UPLOADED_FILE_NAME = await Rest_API.upload_File_Object(INPUT_FILE_SEL.files[0])
+  alert(`The requested file '${UPLOADED_FILE_NAME}' was successfully uploaded !`)
+  await update_Owned_File_Names()
 }
+
+update_Owned_File_Names = Wrap.Wrap_With_Try_Alert_Promise(update_Owned_File_Names)
+on_Upload_File_Form_Submited = Wrap.Wrap_With_Try_Alert_Promise(on_Upload_File_Form_Submited)
 
 main()
--- static/js/lib/wrap.js
+++ static/js/lib/wrap.js
@@ -1,14 +1,12 @@
 /** 함수들을 특정한 형식으로 감싸기 위해서 */
 class Wrap
 {
-  /** 특정 함수를 Try~Catch~Alert 형태로 감싸면서 실행시키기 위해서 */
-  static async Execute_With_Try_Alert_Promise(async_callback)
+  /** 특정 함수를 Try~Catch~Alert 형태로 감싸기 위해서 */
+  static Wrap_With_Try_Alert_Promise(async_callback)
   {
-    try {
-      await async_callback()
-    }
-    catch(e) {
-      alert(e)
+    return async (...args) => {
+      try { await async_callback(...args) }
+      catch(e) { alert(e) }
     }
   }   
 }
