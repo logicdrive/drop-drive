9ce0e891e8e3eb3b0b9dcf4a959f4fe095878a0b
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-21 16:11:22 +0000

    directory.js 라우터에 있던 database 조회관련 코드를 firebase_api.js로 분리시킨 리팩토링

--- module/firebase_api.js
+++ module/firebase_api.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
-import { getFirestore, collection, addDoc } from "firebase/firestore"
+import { getFirestore, collection, getDocs, addDoc, query, where } from "firebase/firestore"
 import { getStorage, ref, uploadString } from "firebase/storage"
 
 const FIREBASE_CONFIG = {
@@ -29,6 +29,28 @@ class Firebase_Api
   static async upload_To_Database(collection_path, json_data)
   {
     await addDoc(collection(FIREBASE_STORE, collection_path), json_data)
+  }
+
+  /** 파이어베이스의 Database에 쿼리를 보내기 위해서
+  *
+  * query_To_Data("file_meta_datas", [["where", "owner", "==", "test"]])
+  *
+  * // "file_meta_datas" 라는 이름의 Collection 경로에 owner가 test와 매칭되는 Json 데이터 리스트를 반환시킴
+  */
+  static async query_To_Database(collection_path, querys)
+  {
+    let doc_querys = []
+    for(let query of querys)
+    {
+        if(query.length != 4) throw new Error(`요청하는 쿼리의 길이가 일치하지 않습니다! : ${query}`)
+        if(query[0] == "where") doc_querys.push(where(query[1], query[2], query[3]))
+        else throw new Error(`요청하는 쿼리의 타입이 유효하지 않습니다! : ${query}`)
+    }
+    
+    const DOC_QUERY = query(collection(FIREBASE_STORE, collection_path), ...doc_querys)
+    const QUERY_SNAP_SHOT = await getDocs(DOC_QUERY)
+    const DOC_RESULTS = QUERY_SNAP_SHOT.docs.map((doc) => doc.data())
+    return DOC_RESULTS
   }
 
   /** 파이어베이스의 Storage에 String형식의 데이터를 업로드시키기 위해서 
--- routers/api/v1/directory.js
+++ routers/api/v1/directory.js
@@ -1,15 +1,12 @@
 import express from "express"
-import { firebase_store } from "../../../module/firebase.js"
-import { getDocs, collection, query, where } from "firebase/firestore"
+import Firebase_Api from "../../../module/firebase_api.js"
 import Wrap from "../../../module/wrap.js"
 
 async function get_Router_callback(_, res)
 {
   const USER_AUTH = "Sin"
 
-  const DOC_QUERY = query(collection(firebase_store, "file_meta_datas"), where("owner", "==", USER_AUTH))
-  const QUERY_SNAP_SHOT = await getDocs(DOC_QUERY)
-  const DOC_RESULTS = QUERY_SNAP_SHOT.docs.map((doc) => doc.data())
+  const DOC_RESULTS = await Firebase_Api.query_To_Database("file_meta_datas", [["where", "owner", "==", USER_AUTH]])
   const FILE_NAMES = DOC_RESULTS.map((doc_result) => doc_result.file_name + "." + doc_result.file_ext)
   
   return res.json({is_error:false, file_names:FILE_NAMES})
