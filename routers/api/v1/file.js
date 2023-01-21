6a15297e52b8a0efdfa7b48470fdea1bc1ac91e3
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-21 14:59:49 +0000

    file.js 모듈부분의 라우터들이 별도로 분리된 함수를 가지도록 리팩토링

--- routers/api/v1/file.js
+++ routers/api/v1/file.js
@@ -1,50 +1,52 @@
 import express from "express"
-import { firebase_store, firebase_storage } from "../../../module/firebase.js"
-import { collection, addDoc } from "firebase/firestore"
 import { v4 } from "uuid"
 import { ref, uploadString } from "firebase/storage"
+import { collection, addDoc } from "firebase/firestore"
+import { firebase_store, firebase_storage } from "../../../module/firebase.js"
+import Wrap from "../../../module/wrap.js"
+import Params_Check from "../../../module/params_check.js"
+
+async function put_Router_callback(req, res)
+{
+  const USER_AUTH = "Sin"
+  const ACCEPT_FILE_EXTS = ["txt"]
+  
+  const [FILE_NAME, FILE_EXT] = req.body.file_name.toLowerCase().split(".")
+  if(!ACCEPT_FILE_EXTS.includes(FILE_EXT))
+  {
+    res.json({is_error:true, error_message:"Passed file's extension was not accepted."})
+    return
+  }
+
+  const FILE_UUID = v4()
+  await addDoc(collection(firebase_store, "file_meta_datas"), {
+    "file_name":FILE_NAME,
+    "file_ext":FILE_EXT,
+    "file_uuid":FILE_UUID,
+    "owner":USER_AUTH
+  })
+  await uploadString(ref(firebase_storage, `files/${FILE_UUID}`), req.body.file_url)
+  
+  res.json({is_error:false, error_message:""})
+}
+
+async function get_Router_callback(req, res)
+{
+  res.send('[MOCK] 파일 다운로드 처리')
+}
+
+async function delete_Router_callback(req, res)
+{
+  res.send('[MOCK] 파일 삭제 처리')
+}
+
+put_Router_callback = Wrap.Wrap_With_Try_Res_Promise(put_Router_callback)
+get_Router_callback = Wrap.Wrap_With_Try_Res_Promise(get_Router_callback)
+delete_Router_callback = Wrap.Wrap_With_Try_Res_Promise(delete_Router_callback)
 
 const router = express.Router()
-
-
-router.get('/', (req, res) => {
-    res.send('[MOCK] 파일 다운로드 처리')
-})
-
-router.put('/', async (req, res) => {
-    try
-    { 
-      const USER_AUTH = "Sin"
-      const ACCEPT_FILE_EXTS = ["txt"]
-      
-      const [FILE_NAME, FILE_EXT] = req.body.file_name.toLowerCase().split(".")
-      if(!ACCEPT_FILE_EXTS.includes(FILE_EXT))
-      {
-        res.json({is_error:true, error_message:"Passed file's extension was not accepted."})
-        return
-      }
-
-      const FILE_UUID = v4()
-      await addDoc(collection(firebase_store, "file_meta_datas"), {
-        "file_name":FILE_NAME,
-        "file_ext":FILE_EXT,
-        "file_uuid":FILE_UUID,
-        "owner":USER_AUTH
-      })
-      await uploadString(ref(firebase_storage, `files/${FILE_UUID}`), req.body.file_url)
-      
-      res.json({is_error:false, error_message:""})
-    }
-    catch(e)
-    {
-      console.log(e)
-      res.json({is_error:true, error_message:"Internal server error was occurred."})
-    }
-})
-
-router.delete('/', (req, res) => {
-    res.send('[MOCK] 파일 삭제 처리')
-})
-
+router.put('/', put_Router_callback)
+router.get('/', get_Router_callback)
+router.delete('/', delete_Router_callback)
 
 export default router
