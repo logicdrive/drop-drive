9337d2836240d8f9f8a522cfaadaedf7126198c4
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-22 09:49:33 +0000

    directory.js 라우터에서 불필요한 return 구문 삭제

--- routers/api/v1/directory.js
+++ routers/api/v1/directory.js
@@ -10,7 +10,7 @@ async function get_Router_callback(_, res)
   const DOC_RESULTS = await Firebase_Api.query_To_Database("file_meta_datas", [["where", "owner", "==", USER_AUTH]])
   const FILE_NAMES = DOC_RESULTS.map((doc_result) => doc_result.file_name + "." + doc_result.file_ext)
   
-  return res.json({is_error:false, file_names:FILE_NAMES})
+  res.json({is_error:false, file_names:FILE_NAMES})
 }
 
 get_Router_callback = Wrap.Wrap_With_Try_Res_Promise(get_Router_callback)
