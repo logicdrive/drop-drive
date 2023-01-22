731492d56496216f36a9b762f6cbc3acccc10ddb
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-22 09:59:41 +0000

    user_auth 부분의 에러처리가 라우터로 Wrap 되도록 수정하는 리팩토링

--- routers/api/v1/auth/user_auth.js
+++ routers/api/v1/auth/user_auth.js
@@ -1,10 +1,15 @@
 import express from "express"
 import { firebase_auth } from "../../../../module/firebase.js"
+import Wrap from "../../../../module/wrap.js"
+
+async function get_Router_callback(_, res)
+{
+  res.json({user_auth: (firebase_auth.currentUser) ? firebase_auth.currentUser.email : null})
+}
+
+get_Router_callback = Wrap.Wrap_With_Try_Res_Promise(get_Router_callback)
 
 const router = express.Router()
-
-router.get('/', (_, res) => {
-    res.json({user_auth: (firebase_auth.currentUser) ? firebase_auth.currentUser.email : null})
-})
+router.get('/', get_Router_callback)
 
 export default router
