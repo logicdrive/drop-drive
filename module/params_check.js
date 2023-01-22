83aeffc5b12a78056029c6a049bcdae7a917c818
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-22 10:37:13 +0000

    params_check에서 오표기된 함수명 수정 리팩토링

--- module/params_check.js
+++ module/params_check.js
@@ -16,11 +16,11 @@ class Params_Check
 
   /** 주어진 params 에서 특정 para가 특정 리스트들 중에 한 요소를 포함하고 있지 않은 경우 예외를 일으키기 위해서
   *
-  * Para_is_contatins(req, [["type", ["title", "image", "index"]]])
+  * Para_is_contains(req, [["type", ["title", "image", "index"]]])
   *
   * // req.query 에서 type이 ["title", "image", "index"] 중에서 아무것도 속하지 않은 경우 `The 'type' parameter has unsuitable value !`라는 예외를 발생시킴
   */
-  static Para_is_contatins(params, para_infos_to_check)
+  static Para_is_contains(params, para_infos_to_check)
   {
     for(let para_info of para_infos_to_check)
       if(!para_info[1].includes(params[para_info[0]]))
