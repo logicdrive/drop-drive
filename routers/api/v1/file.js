3fb43f9aa94bc2ef62ace9af638b2397affa9c3f
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-23 03:53:34 +0000

    파일 생성시각 구하는 부분을 datetime 라이브러리로 분리시킴

--- routers/api/v1/file.js
+++ routers/api/v1/file.js
@@ -3,10 +3,8 @@ import Firebase_Api from "../../../module/firebase_api.js"
 import UUID from "../../../module/uuid.js"
 import Wrap from "../../../module/wrap.js"
 import Params_Check from "../../../module/params_check.js"
+import Datetime from "../../../module/datetime.js"
 import get_Router_Callback_Temp from "../../temp/file.js"
-
-import moment from "moment"
-import moment_timezone from "moment-timezone"
 
 // 주어진 파일의 메타데이터를 파이어베이스에, 파일 URL을 파이어스토어에 업로드시키기 위해서
 async function put_Router_callback(req, res)
@@ -20,9 +18,7 @@ async function put_Router_callback(req, res)
   const [FILE_NAME, FILE_EXT] = FILE_NAME_EXT.toLowerCase().split(".")
   if(!ACCEPT_FILE_EXTS.includes(FILE_EXT)) throw new Error("Passed file's extension was not accepted.")
 
-  moment.tz.setDefault("Asia/Seoul")
-  const CURRENT_TIME_STR = moment().format('YYYY-MM-DD HH:mm:ss')
-  
+  const CURRENT_TIME_STR = Datetime.timezone_Date_Str()
   const FILE_UUID = UUID.get_UUID()
   await Firebase_Api.upload_To_Database("file_meta_datas", {
     "file_name":FILE_NAME,
