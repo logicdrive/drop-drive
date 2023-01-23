1aa9ad0e658b38a0b89edea2af56312c59d2771e
최효승 <37767318+lucky6030@users.noreply.github.com>
2023-01-23 16:49:30 +0000

    Firebase Storage에서 uuid를 통해 파일 다운 받기

--- static/js/temp/main_temp.js
+++ static/js/temp/main_temp.js
@@ -1,5 +1,16 @@
 async function on_Click_File_Index_Download_Btn_Temp(e)
 {
   const FILE_NAME = e.path[1].querySelector("a").textContent
-  alert(`[MOCK TEMP] ${FILE_NAME}에 대한 다운로드 요청이 이루어져야함`)
+  if(!confirm(`Do you want to download the '${FILE_NAME}' file?`)) return
+  
+  const DOWNLOADED_FILE_URL = (await Rest_API.request_With_Error_Check(`/api/v1/file?file_name=${FILE_NAME}`, "GET")).download_url
+  //파일 다운로드를 받기 위하여 
+  const anchorElement = document.createElement('a')
+  document.body.appendChild(anchorElement)
+  anchorElement.download = FILE_NAME
+  anchorElement.href = DOWNLOADED_FILE_URL
+  anchorElement.click()
+  document.body.removeChild(anchorElement)
+  
+  alert(`The '${FILE_NAME}' file was successfully downloaded !`)
 }
