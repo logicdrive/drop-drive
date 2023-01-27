32e7fdd76fa024a539fcf5e673c61ba65099d7b8
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-27 11:07:27 +0000

    들여쓰기 일관성을 위한 리팩토링

--- static/js/lib/file.js
+++ static/js/lib/file.js
@@ -4,12 +4,12 @@ class File
   /** 파일 객체에 대한 URL 주소를 반환시키기 위해서 */
   static read_Data_Url(data_file)
   {
-      return new Promise((resolve) => {
-        const FILE_READER = new FileReader()
-        FILE_READER.onloadend = (finish_event) => {
-          resolve(finish_event.currentTarget.result)
-        }
-        FILE_READER.readAsDataURL(data_file)
-      })
+    return new Promise((resolve) => {
+      const FILE_READER = new FileReader()
+      FILE_READER.onloadend = (finish_event) => {
+        resolve(finish_event.currentTarget.result)
+      }
+      FILE_READER.readAsDataURL(data_file)
+    })
   }
 }
