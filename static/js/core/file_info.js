a29c0cfcb2c7f0f7acb5875005e4a51a01cf3525
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-27 11:27:21 +0000

    얻은 DATA URL에서 데이터를 추출해서 유저에게 표시하는 로직을 Element 라이브러리로 분리시키는 리팩토링

--- static/js/core/file_info.js
+++ static/js/core/file_info.js
@@ -1,13 +1,8 @@
 async function main()
 {
   const FILE_NAME = Browser.url_Query_Param('file_name')
-  const DATA_URL = (await Rest_API.request_With_Error_Check(`/api/v1/file?file_name=${FILE_NAME}`, "GET")).data_url
-
-  const FILE_CONTENT = atob(DATA_URL.split(',')[1])
-
-  const ContentElement = document.createElement('div')
-  ContentElement.innerText = FILE_CONTENT
-  document.body.appendChild(ContentElement)
+  const FILE_URL = await Rest_API.get_File_Object_Data_URL(FILE_NAME)
+  await Element.add_Text_Content_By_Data_Url(document.body, FILE_URL)
 }
 main = Wrap.Wrap_With_Try_Alert_Promise(main)
 
--- static/js/lib/element.js
+++ static/js/lib/element.js
@@ -8,4 +8,13 @@ class Element
         sel.onclick = trigger_event
     })
   }
+
+  /** 주어진 데이터 URL에서 텍스트 데이터를 추출해서 주어진 부모 엘리먼트에 그 내용을 추가시키기 위해서 */
+  static add_Text_Content_By_Data_Url(parent_element, data_url)
+  {
+    const FILE_CONTENT = atob(data_url.split(',')[1])
+    const CONTENT_ELEMENT = document.createElement('div')
+    CONTENT_ELEMENT.innerText = FILE_CONTENT
+    parent_element.appendChild(CONTENT_ELEMENT)
+  }
 }
