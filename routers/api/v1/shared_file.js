1b96daccb1545086c681bc7f1a1d11f1e4d6a410
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-28 23:20:55 +0000

    file_share_id가 서버로 전송되고, data_url을 받을 수 있도록 클라이언트쪽 추가

--- routers/api/v1/shared_file.js
+++ routers/api/v1/shared_file.js
@@ -1,9 +1,17 @@
 import express from "express"
+import Firebase_Api from "../../../module/firebase_api.js"
 import Wrap from "../../../module/wrap.js"
+import Params_Check from "../../../module/params_check.js"
 
 async function get_Router_callback(req, res)
 {
-  res.json({is_error:false, data_url:"", message:"[MOCK] 공유된 DATA URL을 반환시켜야함"})
+  Params_Check.Para_is_null_or_empty(req.query, ["file_share_id"])
+  
+  const USER_AUTH = Firebase_Api.user_Auth()
+  const {file_share_id:FILE_SHARE_ID} = req.query
+  
+  console.log(FILE_SHARE_ID)
+  res.json({is_error:false, data_url:"data:text/plain;base64,VEVTVCAxCg=="})
 }
 get_Router_callback = Wrap.Wrap_With_Try_Res_Promise(get_Router_callback)
 
--- static/js/core/file_share.js
+++ static/js/core/file_share.js
@@ -1,6 +1,8 @@
 async function main()
 {
-
+  const FILE_SHARE_ID = Browser.url_Query_Param('file_share_id')
+  const FILE_URL = (await Rest_API.request_With_Error_Check(`/api/v1/shared_file?file_share_id=${FILE_SHARE_ID}`, "GET")).data_url
+  await Element.add_Text_Content_By_Data_Url(document.body, FILE_URL)
 }
 main = Wrap.Wrap_With_Try_Alert_Promise(main)
 
