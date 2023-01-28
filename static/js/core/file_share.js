ab9c72f142180f79c7e4c5c068dc9efd0c426545
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-28 23:46:23 +0000

    파일 공유 UUID에 대한 DATA URL을 서버에 요청하는 로직을 rest_api로 분리시키는 리팩토링

--- static/js/core/file_share.js
+++ static/js/core/file_share.js
@@ -1,7 +1,7 @@
 async function main()
 {
   const FILE_SHARE_ID = Browser.url_Query_Param('file_share_id')
-  const FILE_URL = (await Rest_API.request_With_Error_Check(`/api/v1/shared_file?file_share_id=${FILE_SHARE_ID}`, "GET")).data_url
+  const FILE_URL = await Rest_API.get_Share_File_Object_Data_URL(FILE_SHARE_ID)
   await Element.add_Text_Content_By_Data_Url(document.body, FILE_URL)
 }
 main = Wrap.Wrap_With_Try_Alert_Promise(main)
