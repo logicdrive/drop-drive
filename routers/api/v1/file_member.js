7353cf83e2515adebe5d8a44bfc0bafd58bcabb5
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-28 05:51:15 +0000

    file_member 에서 사용하지 않는 메소드 함수 삭제

--- routers/api/v1/file_member.js
+++ routers/api/v1/file_member.js
@@ -28,13 +28,6 @@ async function put_Router_callback(req, res)
 }
 put_Router_callback = Wrap.Wrap_With_Try_Res_Promise(put_Router_callback)
 
-async function get_Router_callback(req, res)
-{
-  res.send('[MOCK] 특정 파일에 대한 접근 권한 계정 목록을 얻음')
-}
-get_Router_callback = Wrap.Wrap_With_Try_Res_Promise(get_Router_callback)
-
 const router = express.Router()
 router.put('/', put_Router_callback)
-router.get('/', get_Router_callback)
 export default router
