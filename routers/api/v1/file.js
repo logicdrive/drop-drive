48c415be5f8ef178b82e37aa1e7a2dba77d8432f
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-31 07:24:37 +0000

    폴더 관리 기능 추가 기반 작업 : 파일 접근 권한 추가 로직이 file path, type을 고려하도록 수정

--- routers/api/v1/file.js
+++ routers/api/v1/file.js
@@ -66,7 +66,7 @@ async function delete_Router_callback(req, res)
   const FILE_UUID_TO_DELETE = QUERY_RESULT_FILE_INFOS[0].file_uuid
   
   await Firebase_Api.delete_From_Storage(`${USER_AUTH}/${FILE_UUID_TO_DELETE}`)
-  await Firebase_Api.delete_From_Database(`app/${USER_AUTH}/share_links`, [["where", "file_uuid", "==", FILE_UUID_TO_DELETE]], false)
+  await Firebase_Api.delete_From_Database(`app/global/share_links`, [["where", "file_uuid", "==", FILE_UUID_TO_DELETE]], false)
   await Firebase_Api.delete_From_Database(`app/${USER_AUTH}/share_auths`, [["where", "file_uuid", "==", FILE_UUID_TO_DELETE]], false)
   await Firebase_Api.delete_From_Database(`app/${USER_AUTH}/file_meta_datas`, [["where", "file_uuid", "==", FILE_UUID_TO_DELETE]])
   
