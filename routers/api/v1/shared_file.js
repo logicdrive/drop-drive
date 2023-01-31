38d1c8567995f02bd786de6ebeea9ed9d3980d99
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-31 07:38:57 +0000

    폴더 관리 기능 추가 기반 작업 : 공유 링크에서 파일 내용을 출력하는 로직이 file path, type을 고려하도록 수정

--- routers/api/v1/shared_file.js
+++ routers/api/v1/shared_file.js
@@ -11,21 +11,21 @@ async function get_Router_callback(req, res)
   
   const {file_share_id:FILE_SHARE_ID} = req.query
 
-  const QUERY_RESULT_SHARE_LINKS = await Firebase_Api.query_To_Database("share_links", [["where", "file_share_id", "==", FILE_SHARE_ID]])
+  const QUERY_RESULT_SHARE_LINKS = await Firebase_Api.query_To_Database(`app/global/share_links`, [["where", "file_share_id", "==", FILE_SHARE_ID]])
   if(QUERY_RESULT_SHARE_LINKS.length == 0) throw new Error("The file id matched for given share link id is not searched !")
   const FILE_UUID = QUERY_RESULT_SHARE_LINKS[0].file_uuid
+  const SHARE_FILE_AUTH = QUERY_RESULT_SHARE_LINKS[0].owner
 
-  const QUERY_RESULT_FILE_INFOS = await Firebase_Api.query_To_Database("file_meta_datas", [["where", "file_uuid", "==", FILE_UUID]])
+  const QUERY_RESULT_FILE_INFOS = await Firebase_Api.query_To_Database(`app/${SHARE_FILE_AUTH}/file_meta_datas`,  [["where", "file_uuid", "==", FILE_UUID]])
   if(QUERY_RESULT_FILE_INFOS.length == 0) throw new Error("The file meta datas is not searched !")
-  const FILE_OWNER = QUERY_RESULT_FILE_INFOS[0].owner
 
-  const QUERY_RESULT_SHARE_AUTHS = await Firebase_Api.query_To_Database("share_auths", [["where", "file_uuid", "==", FILE_UUID]])
+  const QUERY_RESULT_SHARE_AUTHS = await Firebase_Api.query_To_Database(`app/${SHARE_FILE_AUTH}/share_auths`, [["where", "file_uuid", "==", FILE_UUID]])
   const FILE_SHARE_AUTHS = QUERY_RESULT_SHARE_AUTHS.map((doc_result) => doc_result.email_auth)
 
-  if(!(USER_AUTH == FILE_OWNER || FILE_SHARE_AUTHS.includes(USER_AUTH)))
+  if(!(USER_AUTH == SHARE_FILE_AUTH || FILE_SHARE_AUTHS.includes(USER_AUTH)))
     throw new Error("The user auth is not suitable !")
 
-  const DATA_URL = await Firebase_Api.string_data_From_Storage(`files/${FILE_UUID}`)
+  const DATA_URL = await Firebase_Api.string_data_From_Storage(`${SHARE_FILE_AUTH}/${FILE_UUID}`)
   res.json({is_error:false, data_url:DATA_URL})
 }
 get_Router_callback = Wrap.Wrap_With_Try_Res_Promise(get_Router_callback)
