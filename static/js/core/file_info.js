cf2604226c8b3d7a2e137eaffb3abbdf6de82db3
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-02-11 10:21:31 +0000

    파일 정보 확인 기능/기능 추가 : 다운로드 기능을 추가시킴

--- static/js/core/file_info.js
+++ static/js/core/file_info.js
@@ -1,3 +1,4 @@
+let FILE_URL = ""
 async function main() {
   const FILE_NAME = Browser.url_Query_Param('file_name')
   const WORK_DIR_PATH = Browser.url_Query_Param('work_dir_path')
@@ -6,9 +7,19 @@ async function main() {
   const FILE_INFO_SEL = document.querySelector("#file_info")
   FILE_NAME_SEL.textContent = FILE_NAME
 
-  const FILE_URL = await Rest_API.get_File_Object_Data_URL(FILE_NAME, WORK_DIR_PATH)
+  FILE_URL = await Rest_API.get_File_Object_Data_URL(FILE_NAME, WORK_DIR_PATH)
   await Element.add_Text_Content_By_Data_Url(FILE_INFO_SEL, FILE_URL)
+
+  document.querySelector("#file_download").onclick = on_Click_File_Download
 }
 main = Wrap.Wrap_With_Try_Alert_Promise(main)
+
+async function on_Click_File_Download()
+{
+  const FILE_NAME = Browser.url_Query_Param('file_name')
+  
+  await Browser.download_File(FILE_URL, FILE_NAME)
+}
+on_Click_File_Download = Wrap.Wrap_With_Try_Alert_Promise(on_Click_File_Download)
 
 main()
--- static/js/core/file_share.js
+++ static/js/core/file_share.js
@@ -1,14 +1,23 @@
+let OBJECT_DATA_INFO = null
 async function main()
 {
   const FILE_SHARE_ID = Browser.url_Query_Param('file_share_id')
-  const OBJECT_DATA_INFO = await Rest_API.get_Share_File_Object_Data_Info(FILE_SHARE_ID)
+  OBJECT_DATA_INFO = await Rest_API.get_Share_File_Object_Data_Info(FILE_SHARE_ID)
 
   const FILE_NAME_SEL = document.querySelector("#file_name")
   const FILE_INFO_SEL = document.querySelector("#file_info")
   FILE_NAME_SEL.textContent = OBJECT_DATA_INFO.file_name
   
   await Element.add_Text_Content_By_Data_Url(FILE_INFO_SEL, OBJECT_DATA_INFO.data_url)
+
+  document.querySelector("#file_download").onclick = on_Click_File_Download
 }
 main = Wrap.Wrap_With_Try_Alert_Promise(main)
+
+async function on_Click_File_Download()
+{
+  await Browser.download_File(OBJECT_DATA_INFO.data_url, OBJECT_DATA_INFO.file_name)
+}
+on_Click_File_Download = Wrap.Wrap_With_Try_Alert_Promise(on_Click_File_Download)
 
 main()
