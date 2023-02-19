b8f97e50afd550745f59da7006c6d589a15402c6
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-02-19 10:40:28 +0000

    zip 커맨드를 사용할 수 있도록 설정파일 수정

--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-18_x
+		pkgs.zip
+  pkgs.nodejs-18_x
         pkgs.nodePackages.typescript-language-server
         pkgs.nodePackages.yarn
         pkgs.replitPackages.jest
