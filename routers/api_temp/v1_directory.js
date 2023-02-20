82a485ad4323657d6fa275586276b6b57a3b7e07
최효승 <37767318+lucky6030@users.noreply.github.com>
2023-02-20 06:43:01 +0000

    디렉토리 다운로드 기능 구현

--- routers/api_temp/v1_directory.js
+++ routers/api_temp/v1_directory.js
@@ -2,6 +2,10 @@ import Firebase_Service from "../../module/firebase_service.js"
 import Wrap from "../../module/wrap.js"
 import Params_Check from "../../module/params_check.js"
 
+import UUID from "../../module/uuid.js"
+import fs from "fs"
+import { exec } from "child_process"
+
 // 주어진 디렉토리에 대한 DATA URL을 반환받기 위해서(오버라이딩)
 async function post_Router_Callback_Overide(req, res)
 {
@@ -10,8 +14,70 @@ async function post_Router_Callback_Overide(req, res)
     = Params_Check.Para_is_null_or_empty(req.body, ["file_name", "work_dir_path"])
 
   const TEST_DATA_URL = `[TEST DATA URL] FILE_NAME : ${FILE_NAME} / WORK_DIR_PATH : ${WORK_DIR_PATH} / USER_AUTH : ${USER_AUTH}`
-  res.json({is_error:false, data_url:TEST_DATA_URL})
+
+  const FOLDER_UUID = UUID.get_UUID()
+  const DOWNLOAD_FOLDER_PATH = `./downloads/${FOLDER_UUID}`
+  fs.mkdirSync(DOWNLOAD_FOLDER_PATH)
+
+  await make_Directory_Recursively(DOWNLOAD_FOLDER_PATH, WORK_DIR_PATH, USER_AUTH)
+
+  const ZIP_PATH = `./downloads/${FOLDER_UUID}.zip`
+  await System.execute_Shell_Command(`cd ${DOWNLOAD_FOLDER_PATH};zip -r ../${FOLDER_UUID}.zip ./*`)
+
+  const ZIP_DATA_BASE64 = fs.readFileSync(ZIP_PATH, {encoding: 'base64'})
+  const ZIP_DATA_URL = "data:file/zip;base64," + ZIP_DATA_BASE64
+
+  fs.rmSync(DOWNLOAD_FOLDER_PATH, {recursive: true, force: true})
+  fs.rmSync(ZIP_PATH, {force: true})
+  
+  res.json({is_error:false, file_name: "file", data_url:ZIP_DATA_URL})
 }
+
+async function make_Directory_Recursively(download_folder_path, work_dir_path, user_auth) {
+  const RESULT_TARGET_DIRECTORY_INFOS = await Firebase_Service.directory_File_Infos(work_dir_path, user_auth)
+  if(RESULT_TARGET_DIRECTORY_INFOS.length == 0) return
+
+  for(let sub_content_info of RESULT_TARGET_DIRECTORY_INFOS) {
+    if(sub_content_info.type == "directory") {
+      const DOWNLOAD_SUB_FOLDER_PATH = `${download_folder_path}/${sub_content_info.file_name}`
+      const WORK_DIR_PATH = work_dir_path + sub_content_info.file_name + '/'
+      fs.mkdirSync(DOWNLOAD_SUB_FOLDER_PATH)
+      make_Directory_Recursively(DOWNLOAD_SUB_FOLDER_PATH, WORK_DIR_PATH, user_auth)
+    }
+    if(sub_content_info.type == "file") {
+      const FILE_INFO = sub_content_info.file_name
+      const [FILE_NAME, FILE_EXT] = FILE_INFO.split(".")
+      const DATA_URL = await Firebase_Service.file_Data_URL(FILE_NAME, FILE_EXT, work_dir_path, user_auth)
+      
+      const FILE_CONTENT = atob(DATA_URL.split(',')[1])
+      const DOWNLOAD_FILE_PATH = `${download_folder_path}/${FILE_INFO}`
+      fs.writeFileSync(DOWNLOAD_FILE_PATH, FILE_CONTENT)
+    }
+  }
+}
+
+/** 시스템관련 명령어를 실행시킨기 위한 라이브러리 */
+class System
+{
+  /** 주어진 쉘 커멘드를 실행시키기 위해서 */
+  static execute_Shell_Command(shell_command)
+  {
+    return new Promise((resolve, reject) => {
+      exec(shell_command, (error, stdout, stderr) => {
+        if(error) reject(stderr)
+        else resolve(stdout)
+      })
+    })
+  }
+
+  /** 서버 요청 과부화등을 방지하는 이유로 의도적으로 처리 속도를 조절하기 위해서 */
+  static sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+}        
+
 post_Router_Callback_Overide = Wrap.Wrap_With_Try_Res_Promise(post_Router_Callback_Overide)
 
 export default post_Router_Callback_Overide
--- static/js/temp/core_main.js
+++ static/js/temp/core_main.js
@@ -10,6 +10,58 @@ async function on_Click_Download_Directory_Btn_Overide(e)
   })
   if(REQ_RESULT.is_error)
     throw new Error(`Sorry, Some error was happened...\nError Message : ${REQ_RESULT.message}`)
-  
-  alert(`[MOCK] 주어진 디렉토리 다운로드 기능. TEST DATA URL => "${REQ_RESULT.data_url}"`)   
+
+  const TITLE_NAME = REQ_RESULT.file_name
+  const ZIP_DATA_URL = REQ_RESULT.data_url
+  Download_Manager.download_Files(ZIP_DATA_URL, TITLE_NAME)
+  //alert(`[MOCK] 주어진 디렉토리 다운로드 기능. TEST DATA URL => "${REQ_RESULT.data_url}"`)   
+}
+
+/** 다운로드 UI 정보들을 일괄적으로 관리하기 위해서 */
+class Download_Manager
+{
+  static _current_download_infos = []
+
+  /** 주어진 웹툰 정보들을 기반으로 다운로드를 수행하기 위해서 */
+  static async download_Files(ZIP_DATA_URL, title_name)
+  {  
+    const ZIP_NAME = Download_Manager._get_Download_Zip_Name(title_name)
+    Download_Manager._add_To_Download_List(ZIP_NAME)
+    //Download_Manager._update_download_UI()
+    //Index_Manager.init_Index_Info()
+
+    console.log(ZIP_DATA_URL)
+    console.log(ZIP_NAME)
+    await Browser.download_File(ZIP_DATA_URL, ZIP_NAME)
+    
+    Download_Manager._delete_From_Download_List(ZIP_NAME)
+    //Download_Manager._update_download_UI()
+    //Process_Visible_Manager.change_Process_Visible_Level(3)
+  }
+
+  /** 주어진 타이틀 이름을 이용해서 사용할 zip 이름을 생성시키기 위해서 */
+  static _get_Download_Zip_Name(title_name)
+  {
+    const ZIP_NAME = `${title_name}.zip`
+    return ZIP_NAME
+  }
+
+  /** 다운로드 리스트에 다운로드시킬 파일명을 추가시키기 위해서 */
+  static _add_To_Download_List(file_name)
+  {
+    Download_Manager._current_download_infos.push(file_name)
+  }
+
+  /** 다운로드 리스트에서 특정 파일명을 삭제시키기 위해서 */
+  static _delete_From_Download_List(file_name)
+  {
+    Download_Manager._current_download_infos = Download_Manager._current_download_infos.filter((download_info) => download_info != file_name)
+  }
+
+  /** 다운로드 관련 UI를 업데이트시키기 위해서 */
+  static _update_download_UI()
+  {
+    const INDEX_RESULT_HTMLS = Download_Manager._current_download_infos.map((download_info) => `<li class="list-group-item">파일명 : ${download_info}<br/>진행상태 : 다운로드 진행중...</li>`)
+    document.querySelector("#download_process_list").innerHTML = INDEX_RESULT_HTMLS.join('\n')
+  }
 }
