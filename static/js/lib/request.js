9e36ea2d5ec4ee08bece729c1abd279f0c02027b
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-03-01 14:30:11 +0000

    zip 다운로드 기능/리팩토링 : Request_Test 클래스를 Request 클래스로 병합시킴

--- static/js/lib/request.js
+++ static/js/lib/request.js
@@ -1,8 +1,30 @@
 /** HTTP 요청처리를 간단하게하기 위한 라이브러리 */
 class Request
 {
-  /** 특정 HTTP 요청 형태에 대해서 JSON Body로 요청하고, JSON 형태로 데이터를 받기 위해서 */
-  static async JSON_Request(url, request_type, json_body={})
+  /** JSON 데이터를 요청하고, 그 결과를 JSON 형태로 받기 위해서 */
+  static async JSON_Request(url, request_type, json_body={}, extra_header_infos={})
+  {
+    const RES_OBJ = await Request.request_Using_JSON(url, request_type, json_body, extra_header_infos)
+    return (RES_OBJ.ok) ? RES_OBJ.json() : {is_error:true, status:RES_OBJ.status}
+  }
+
+  /** text/stream 형태의 SSE 통신 데이터관련 eventStream 객체를 얻기 위해서 */
+  static async create_Event_Stream_JSON_Request(url, request_type, json_body={}, extra_header_infos={})
+  {
+    const RES_OBJ = await Request.request_Using_JSON(url, request_type, json_body, extra_header_infos)
+    return (RES_OBJ.ok) ? RES_OBJ.body.getReader() : {is_error:true, status:RES_OBJ.status}
+  }
+
+  /** 주어진 eventStream 객체의 결과를 한번 얻어서 결과를 반환하기 위해서 */
+  static async use_Event_Stream_Request(event_stream)
+  {
+     const CHUNK_DATA = await event_stream.read()    
+     if(CHUNK_DATA.done) return {is_done:true, chunk_data:null}
+     return {is_done:false, chunk_data:new TextDecoder().decode(CHUNK_DATA.value)} 
+  }
+
+  /** JSON 데이터로 서버에 요청하기 위해서 */
+  static async request_Using_JSON(url, request_type, json_body, extra_header_infos)
   {
     const HTTP_REQUEST_TYPES = ["GET", "HEAD", "PUT", "POST", "DELETE", "TRACE", "CONNECT", "OPTIONS"]
     if(!HTTP_REQUEST_TYPES.includes(request_type.toUpperCase()))
@@ -10,11 +32,11 @@ class Request
 
     let request_infos = {
       method: request_type,
-      headers: { "Content-Type": "application/json" }
+      headers: {"Content-Type": "application/json", ...extra_header_infos},
     }
     if(!["GET", "HEAD", "DELETE", "TRACE"].includes(request_type))
       request_infos.body = JSON.stringify(json_body)
 
-    return (await fetch(url, request_infos)).json()
+    return fetch(url, request_infos)
   }
 }
--- static/js/temp/core_main.js
+++ static/js/temp/core_main.js
@@ -3,84 +3,39 @@ async function on_Click_Download_Directory_Btn_Overide(e)
 {
   const DIRECTORY_NAME_TO_DOWNLOAD = e.target.closest(".dropdown-menu").getAttribute("file_name")
   const WORK_DIR_PATH = Browser.url_Query_Param('work_dir_path')
-  const READER = await Request_Test.create_Event_Stream_JSON_Request("/api/v1/directory", "POST",  {
+  const READER = await Request.create_Event_Stream_JSON_Request("/api/v1/directory", "POST",  {
        file_name : DIRECTORY_NAME_TO_DOWNLOAD,
        work_dir_path : WORK_DIR_PATH
   })
+
   
-  const object_li = e.target.closest("li")
-  const downloadContainer = $(object_li).next()[0]
-  const downloadProgressElem = downloadContainer.getElementsByClassName("download-progress-bar__progress")[0]
+  const SEL_PROGRESS_BAR_CONTAINER = e.target.closest("li").nextElementSibling
+  const SEL_PROGRESS_BAR = SEL_PROGRESS_BAR_CONTAINER.children[0]
+  SEL_PROGRESS_BAR_CONTAINER.style.display = "block"
+
   
-  downloadContainer.style.display = "block"
-  
-  let res_data = ""
-  const RES_DATA = await Request_Test.use_Event_Stream_Request(READER)
-  const total_progress = Number(RES_DATA.chunk_data)
-  
+  let chunk_data = ""
+  const TOTAL_PROGRESS = Number((await Request.use_Event_Stream_Request(READER)).chunk_data)
   while(true)
   {
-    const RES_DATA = await Request_Test.use_Event_Stream_Request(READER)
+    const RES_DATA = await Request.use_Event_Stream_Request(READER)
     if(RES_DATA.is_done) {
-      downloadContainer.style.display = "none"
-      downloadProgressElem.textContent = "0%"
-      downloadProgressElem.style.width = "0%"
+      SEL_PROGRESS_BAR_CONTAINER.style.display = "none"
+      SEL_PROGRESS_BAR.textContent = "0%"
+      SEL_PROGRESS_BAR.style.width = "0%"
       break
     }
-    res_data = RES_DATA.chunk_data
     
-    if(!isNaN(Number(res_data))) {
-      let percentage = Math.floor(Number(res_data) / total_progress * 100)
-      downloadProgressElem.textContent = `${percentage}%`
-      downloadProgressElem.style.width = `${percentage}%`
-    }
-      
+    chunk_data = RES_DATA.chunk_data
+    if(!isNaN(Number(chunk_data))) {
+      const CURRENT_PERCENTAGE = Math.floor(Number(chunk_data)/TOTAL_PROGRESS*100)
+      SEL_PROGRESS_BAR.textContent = `${CURRENT_PERCENTAGE}%`
+      SEL_PROGRESS_BAR.style.width = `${CURRENT_PERCENTAGE}%`
+    } 
   }
-  const ZIP_DATA_URL = res_data.split(" ")[1]
+
   
+  const ZIP_DATA_URL = chunk_data.split(" ")[1]
   await Browser.download_File(ZIP_DATA_URL, `${DIRECTORY_NAME_TO_DOWNLOAD}.zip`)
-  
   return
-}
-
-class Request_Test
-{
-  /** JSON 데이터를 요청하고, 그 결과를 JSON 형태로 받기 위해서 */
-  static async JSON_Request(url, request_type, json_body={}, extra_header_infos={})
-  {
-    const RES_OBJ = await Request_Test.request_Using_JSON(url, request_type, json_body, extra_header_infos)
-    return (RES_OBJ.ok) ? RES_OBJ.json() : {is_error:true, status:RES_OBJ.status}
-  }
-
-  /** text/stream 형태의 SSE 통신 데이터관련 eventStream 객체를 얻기 위해서 */
-  static async create_Event_Stream_JSON_Request(url, request_type, json_body={}, extra_header_infos={})
-  {
-    const RES_OBJ = await Request_Test.request_Using_JSON(url, request_type, json_body, extra_header_infos)
-    return (RES_OBJ.ok) ? RES_OBJ.body.getReader() : {is_error:true, status:RES_OBJ.status}
-  }
-
-  /** JSON 데이터로 서버에 요청하기 위해서 */
-  static async request_Using_JSON(url, request_type, json_body, extra_header_infos)
-  {
-    const HTTP_REQUEST_TYPES = ["GET", "HEAD", "PUT", "POST", "DELETE", "TRACE", "CONNECT", "OPTIONS"]
-    if(!HTTP_REQUEST_TYPES.includes(request_type.toUpperCase()))
-      throw new Error("request_type에는 알맞은 HTTP 요청타입이 들어가야 합니다!")
-
-    let request_infos = {
-      method: request_type,
-      headers: {"Content-Type": "application/json", ...extra_header_infos},
-    }
-    if(!["GET", "HEAD", "DELETE", "TRACE"].includes(request_type))
-      request_infos.body = JSON.stringify(json_body)
-
-    return fetch(url, request_infos)
-  }
-
-  /** 주어진 eventStream 객체의 결과를 한번 얻어서 결과를 반환하기 위해서 */
-  static async use_Event_Stream_Request(event_stream)
-  {
-     const CHUNK_DATA = await event_stream.read()
-     if(CHUNK_DATA.done) return {is_done:true, chunk_data:null}
-     return {is_done:false, chunk_data:new TextDecoder().decode(CHUNK_DATA.value)} 
-  }
 }
