1bff6635b693454aef07c8736c774db32e80c304
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-03-02 08:27:48 +0000

    zip 다운로드 기능/리팩토링 : 파일 진행률 계산을 서버측으로 이동시켜서 간결화 시킴

--- static/js/temp/core_main.js
+++ static/js/temp/core_main.js
@@ -8,14 +8,15 @@ async function on_Click_Download_Directory_Btn_Overide(e)
        work_dir_path : WORK_DIR_PATH
   })
 
-  
+
+  // .zip 다운로드 진행바를 표시시키기 위해서
   const SEL_PROGRESS_BAR_CONTAINER = e.target.closest("li").nextElementSibling
   const SEL_PROGRESS_BAR = SEL_PROGRESS_BAR_CONTAINER.children[0]
   SEL_PROGRESS_BAR_CONTAINER.style.display = "block"
 
-  
+
+  // .zip 다운로드 진행률을 실시간으로 보여주고, 그 결과를 $chunk_data에 저장시키기 위해서
   let chunk_data = ""
-  const TOTAL_PROGRESS = Number((await Request.use_Event_Stream_Request(READER)).chunk_data)
   while(true)
   {
     const RES_DATA = await Request.use_Event_Stream_Request(READER)
@@ -28,13 +29,13 @@ async function on_Click_Download_Directory_Btn_Overide(e)
     
     chunk_data = RES_DATA.chunk_data
     if(!isNaN(Number(chunk_data))) {
-      const CURRENT_PERCENTAGE = Math.floor(Number(chunk_data)/TOTAL_PROGRESS*100)
-      SEL_PROGRESS_BAR.textContent = `${CURRENT_PERCENTAGE}%`
-      SEL_PROGRESS_BAR.style.width = `${CURRENT_PERCENTAGE}%`
+      SEL_PROGRESS_BAR.textContent = `${Number(chunk_data)}%`
+      SEL_PROGRESS_BAR.style.width = `${Number(chunk_data)}%`
     } 
   }
 
-  
+
+  // 브라우저로 .zip 파일을 다운로드 받기 위해서
   const ZIP_DATA_URL = chunk_data.split(" ")[1]
   await Browser.download_File(ZIP_DATA_URL, `${DIRECTORY_NAME_TO_DOWNLOAD}.zip`)
   return
